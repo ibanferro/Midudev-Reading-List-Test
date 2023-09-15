00dc183aedd686e46cc3aa4387e2c30606e359b5
ibanferro <iban.ferro@gmail.com>
2023-09-15 19:24:36 +0200

    Initial components and routing initiated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { BookComponent } from './components/book/book.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: '/home'},
+  {path: 'home', component: HomeComponent},
+  {path: 'book/:id', component: BookComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
